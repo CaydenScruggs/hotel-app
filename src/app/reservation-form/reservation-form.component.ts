9039804c10787b6bebaa9cdd7fd2ad4d43446eaf
CaydenScruggs <garterscales619@yahoo.com>
2025-01-07 16:44:55 -0500

    1/7 deletion and editing begun

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-form',
@@ -10,7 +13,10 @@ export class ReservationFormComponent implements OnInit {
 
   reservationForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService,
+    private router: Router) {
 
   }
 
@@ -28,7 +34,10 @@ export class ReservationFormComponent implements OnInit {
 
   onSubmit() {
     if(this.reservationForm.valid) {
-      console.log("valid")
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation)
+
+      this.router.navigate(['/list'])
     }
   }
 
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,6 +8,11 @@ export class ReservationService {
 
   private reservations: Reservation[] = [];
 
+  constructor() {
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations? JSON.parse(savedReservations) : [];
+  }
+
   // CRUD
 
   getReservations(): Reservation[] {
@@ -19,15 +24,20 @@ export class ReservationService {
   }
 
   addReservation(reservation: Reservation): void {
+    reservation.id = Date.now().toString();
     this.reservations.push(reservation);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index, 1);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(updatedReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === updatedReservation.id)
+    this.reservations[index] = updatedReservation;
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 }
